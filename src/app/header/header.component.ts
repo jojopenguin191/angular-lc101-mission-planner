16539b78942091ab5e68319691546944f61518d4
jojopenguin191 <107898041+jojopenguin191@users.noreply.github.com>
2023-05-10 19:15:18 -0500

    remove member

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,22 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  missionName: string = "Mars 2030";
-  rocketName: string = "Plasma Max";
+  missionName: string = 'Mars 2030';
+  rocketName: string = 'Plasma Max';
   editingMission: boolean = false;
-  
-  constructor() { }
+  editingRocket: boolean = false;
 
-  ngOnInit() {
-  }
+  constructor() {}
+
+  ngOnInit() {}
 
   updateMission(updatedName: string) {
     this.missionName = updatedName;
     this.editingMission = false;
   }
 
+  updateRocketName(updatedRocketName: string) {
+    this.rocketName = updatedRocketName;
+    this.editingRocket = false;
+  }
 }
